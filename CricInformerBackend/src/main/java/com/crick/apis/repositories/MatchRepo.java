abd73c8394f4f3d24a5292d9b72eddd97d2a7c22
Rushi1116 <kalane1116@gmail.com>
2023-12-25 12:53:16 +0530

    created angular new project

--- CricInformerBackend/src/main/java/com/crick/apis/repositories/MatchRepo.java
+++ CricInformerBackend/src/main/java/com/crick/apis/repositories/MatchRepo.java
@@ -3,7 +3,7 @@ package com.crick.apis.repositories;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+
 
 import com.crick.apis.entities.Match;
 
--- CricInformerBackend/src/main/java/com/crick/apis/services/impl/MatchServiceImpl.java
+++ CricInformerBackend/src/main/java/com/crick/apis/services/impl/MatchServiceImpl.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
-import java.util.Map;
+
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
